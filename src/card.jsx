a0c933539a771241055ae44e6b666d55ac43eab9
sazzy-josh <idahosajoshua61@gmail.com>
2022-05-02 20:43:25 +0100

    add conditional rendering to the delete btn

--- src/card.jsx
+++ src/card.jsx
@@ -1,12 +1,10 @@
 const Card = ({data,handleDelete}) => {
     return ( 
-      
-     
            data.map((user , i) => {
               const {name , email ,id} = user
                 return (
                     <div key={id} className="card">
-                      <img src={`https://robohash.org/bgset_bg2/${name}.png`} alt="robot" />
+                      <img src={`https://robohash.org/bgset_bg2/${name}.png`} alt={`robot_${name}`} />
                       <div className="text-align">
                        <p><strong>{name}</strong></p>
                        <p>{email}</p>
